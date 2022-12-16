1d2dc97af4c3382bef9c2ae86ee2f361d565db9f
Alex Hesketh <alexhesketh@outlook.com>
2022-12-16 01:47:16 +0000

    Improved command line support

--- VSSolutionTools/Program.cs
+++ VSSolutionTools/Program.cs
@@ -1,21 +1,24 @@
-﻿using Microsoft.Build.Construction;
-using Microsoft.Build.Evaluation;
-using System.CommandLine;
-using System.Text;
+﻿using System.CommandLine;
 using VSSolutionTools;
 
-//var root = args.Length > 0 ? args[0] : Directory.GetCurrentDirectory();
-
+// Build Options
 var directoryOption = new Option<DirectoryInfo?>(
-    name: "--directory",
+    name: "-directory",
     description: "The root directory of your solutions and projects");
+directoryOption.AddAlias("-d");
+directoryOption.SetDefaultValue(new DirectoryInfo(Directory.GetCurrentDirectory()));
 
-var rootCommand = new RootCommand("Output a CSV showing all Projects and which Solutions reference them");
-rootCommand.AddOption(directoryOption);
+// Build Commands
+var rootCommand = new RootCommand("Perform bulk operations across solutions");
 
-rootCommand.SetHandler((directoryInfo) =>
+// Generate Report
+var reportCommand = new Command("report", "Output a CSV showing all Projects and which Solutions reference them");
+reportCommand.AddOption(directoryOption);
+reportCommand.SetHandler((directoryInfo) =>
 {
     ProjectSolutionReport.GenerateReport(directoryInfo!.FullName);
 }, directoryOption);
+rootCommand.AddCommand(reportCommand);
 
+// Actually Run the App
 return await rootCommand.InvokeAsync(args);
