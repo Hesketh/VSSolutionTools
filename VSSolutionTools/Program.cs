bb875787f40a62241c6131f89d8473b426a0a546
Alex Hesketh <alexhesketh@outlook.com>
2022-12-16 01:34:57 +0000

    Use command line parsing

--- VSSolutionTools/Program.cs
+++ VSSolutionTools/Program.cs
@@ -1,72 +1,21 @@
 ﻿using Microsoft.Build.Construction;
 using Microsoft.Build.Evaluation;
+using System.CommandLine;
 using System.Text;
+using VSSolutionTools;
 
-var root = args.Length > 0 ? args[0] : Directory.GetCurrentDirectory();
+//var root = args.Length > 0 ? args[0] : Directory.GetCurrentDirectory();
 
-var solutions = Directory.GetFiles(root, "*.sln", SearchOption.AllDirectories);
-var projects = Directory.GetFiles(root, "*.csproj", SearchOption.AllDirectories);
+var directoryOption = new Option<DirectoryInfo?>(
+    name: "--directory",
+    description: "The root directory of your solutions and projects");
 
-// Absolute paths for both
-var solutionsByProject = new Dictionary<string, HashSet<string>>();
-var parsedSolutions = new HashSet<string>();
+var rootCommand = new RootCommand("Output a CSV showing all Projects and which Solutions reference them");
+rootCommand.AddOption(directoryOption);
 
-// Build up our lookup of projects with what solutions they are contained in
-Console.WriteLine($"Parsing {solutions.Length} Solutions and {projects.Length} Projects found in {root}");
-foreach (var solutionPath in solutions)
+rootCommand.SetHandler((directoryInfo) =>
 {
-    var solutionAbsolutePath = Path.GetFullPath(solutionPath);
+    ProjectSolutionReport.GenerateReport(directoryInfo!.FullName);
+}, directoryOption);
 
-    var solutionFile = SolutionFile.Parse(solutionPath);
-    if (solutionFile == null) 
-    {
-        Console.WriteLine($"\tCould not parse solution at {solutionFile}");
-        continue; 
-    }
-
-    foreach (var project in solutionFile.ProjectsInOrder)
-    {
-        if (!solutionsByProject.TryGetValue(project.AbsolutePath, out var solutionsContainingProject))
-        {
-            solutionsContainingProject = new();
-            solutionsByProject.Add(project.AbsolutePath, solutionsContainingProject);
-        }
-        solutionsContainingProject.Add(solutionAbsolutePath);
-    }
-
-    parsedSolutions.Add(solutionAbsolutePath);
-    Console.WriteLine($"\tParsed solution at {solutionFile}");
-}
-
-// Output a CSV
-var csvStringBuilder = new StringBuilder();
-var destination = Path.Combine(root, "CSProjSolutionReport.csv");
-Console.WriteLine($"Generating CSV Report at {destination}");
-
-// Build Header
-csvStringBuilder.Append("project");
-foreach (var solution in parsedSolutions)
-{
-    csvStringBuilder.Append(',');
-    csvStringBuilder.Append(solution);
-}
-csvStringBuilder.AppendLine();
-
-// Output the data
-foreach (var project in projects)
-{
-    if (!solutionsByProject.TryGetValue(project, out var solutionsWithProject))
-    {
-        solutionsWithProject = new();
-    }
-
-    csvStringBuilder.Append(project);
-    foreach (var solution in parsedSolutions)
-    {
-        csvStringBuilder.Append(',');
-        csvStringBuilder.Append(solutionsWithProject.Contains(solution));
-    }
-    csvStringBuilder.AppendLine();
-}
-
-File.WriteAllText(destination, csvStringBuilder.ToString());
+return await rootCommand.InvokeAsync(args);
---
+++ VSSolutionTools/ProjectSolutionReport.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Build.Construction;
+using System.Text;
+
+namespace VSSolutionTools
+{
+    internal static class ProjectSolutionReport
+    {
+        public static void GenerateReport(string directoryPath)
+        {
+            var solutions = Directory.GetFiles(directoryPath, "*.sln", SearchOption.AllDirectories);
+            var projects = Directory.GetFiles(directoryPath, "*.csproj", SearchOption.AllDirectories);
+
+            // Absolute paths for both
+            var solutionsByProject = new Dictionary<string, HashSet<string>>();
+            var parsedSolutions = new HashSet<string>();
+
+            // Build up our lookup of projects with what solutions they are contained in
+            Console.WriteLine($"Parsing {solutions.Length} Solutions and {projects.Length} Projects found in {directoryPath}");
+            foreach (var solutionPath in solutions)
+            {
+                var solutionAbsolutePath = Path.GetFullPath(solutionPath);
+
+                var solutionFile = SolutionFile.Parse(solutionPath);
+                if (solutionFile == null)
+                {
+                    Console.WriteLine($"\tCould not parse solution at {solutionFile}");
+                    continue;
+                }
+
+                foreach (var project in solutionFile.ProjectsInOrder)
+                {
+                    if (!solutionsByProject.TryGetValue(project.AbsolutePath, out var solutionsContainingProject))
+                    {
+                        solutionsContainingProject = new();
+                        solutionsByProject.Add(project.AbsolutePath, solutionsContainingProject);
+                    }
+                    solutionsContainingProject.Add(solutionAbsolutePath);
+                }
+
+                parsedSolutions.Add(solutionAbsolutePath);
+                Console.WriteLine($"\tParsed solution at {solutionFile}");
+            }
+
+            // Output a CSV
+            var csvStringBuilder = new StringBuilder();
+            var destination = Path.Combine(directoryPath, "CSProjSolutionReport.csv");
+            Console.WriteLine($"Generating CSV Report at {destination}");
+
+            // Build Header
+            csvStringBuilder.Append("project");
+            foreach (var solution in parsedSolutions)
+            {
+                csvStringBuilder.Append(',');
+                csvStringBuilder.Append(solution);
+            }
+            csvStringBuilder.AppendLine();
+
+            // Output the data
+            foreach (var project in projects)
+            {
+                if (!solutionsByProject.TryGetValue(project, out var solutionsWithProject))
+                {
+                    solutionsWithProject = new();
+                }
+
+                csvStringBuilder.Append(project);
+                foreach (var solution in parsedSolutions)
+                {
+                    csvStringBuilder.Append(',');
+                    csvStringBuilder.Append(solutionsWithProject.Contains(solution));
+                }
+                csvStringBuilder.AppendLine();
+            }
+
+            File.WriteAllText(destination, csvStringBuilder.ToString());
+        }
+    }
+}
